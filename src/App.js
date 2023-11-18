fcf88144b82191300715746c819817b57f056054
AndrzejGrabarz <andgra01@gmail.com>
2023-11-18 22:24:53 +0100

    Setting up a semantic HTML and meta tags

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import './App.css'; 
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
       Homepage
+      <Nav />
+      <Header/>
+      <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <img src="Red.png" width="200px" height="200px"alt="logo" />
+      <map>Mapa</map>
+      <p>Dane kontaktowe</p>
+      <time>Godziny otwarcia</time>
+      <p>Ploityka prywatnośic</p>
+      <ul>
+        <li><a href="/">Jakieś linki</a></li>
+        <li><a href="/">Jakieś linki</a></li>
+        <li><a href="/">Jakieś linki</a></li>
+      </ul>
+    </div>
+    
+  )
+}
+
+export default Footer

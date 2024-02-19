6fe145a4de3b03588b3e803681c505d4ea66f4e7
AndrzejGrabarz <andgra01@gmail.com>
2024-02-19 01:25:31 +0100

    Setting project

---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav>
+      <img src="" alt="" />
+      <ul>
+        <li><a href="/HOME">HOME</a></li>
+        <li><a href="/ABOUT">ABOUT</a></li>
+        <li><a href="/MENU">MENU</a></li>
+        <li><a href="/RESERVATIONS">RESERVATIONS</a></li>
+        <li><a href="/ORDER online">ORDER online</a></li>
+        <li><a href="/LOGIN">LOGIN</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
